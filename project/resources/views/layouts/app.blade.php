c55de404d2989a495eb83bec6cfdbc597f3f2dc8
muhammad adil <m.adilkhan.com>
2023-01-06 01:15:15 +0500

    tawk to live chat

--- project/resources/views/layouts/app.blade.php
+++ project/resources/views/layouts/app.blade.php
@@ -291,7 +291,7 @@
     (function(){
         var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
         s1.async=true;
-        s1.src='https://embed.tawk.to/63b7121547425128790be223/1gm1htanl';
+        s1.src='https://embed.tawk.to/' + {{ $gs->talkto }} + '/1gm1htanl';
         s1.charset='UTF-8';
         s1.setAttribute('crossorigin','*');
         s0.parentNode.insertBefore(s1,s0);
