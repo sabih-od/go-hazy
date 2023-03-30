847127d161cb190bcbaab32da61f2f00e4aac44b
sabih-od <sheikh.muhammad@olivedigital.com>
2023-03-30 07:00:59 +0500

    bugs fix

--- project/resources/views/frontend/index.blade.php
+++ project/resources/views/frontend/index.blade.php
@@ -197,7 +197,11 @@
             </div>
         </div>
     </section>
-    <section class="proSec">
+
+    {{--BEST SELLERS FOR THE
+    BEST VERSION OF YOU START--}}
+
+    {{--<section class="proSec">
         <div class="container">
             <div class="refreshHeading" data-aos="fade-up">
                 <h1>Top Tranding</h1>
@@ -207,7 +211,7 @@
             </div>
             <div class="row">
                 @foreach($products as $key => $prod)
-                    {{--                    {{ dd($products) }}--}}
+                    --}}{{--                    {{ dd($products) }}--}}{{--
                     @if($key > 7)
                         @break
                     @endif
@@ -237,9 +241,15 @@
                 @endforeach
             </div>
         </div>
-    </section>
+    </section>--}}
 
-    <section class="shirtSec">
+    {{--BEST SELLERS FOR THE
+    BEST VERSION OF YOU END--}}
+
+
+    {{--SHIRT SECTION START--}}
+
+    {{--<section class="shirtSec">
         <img src="{{asset('assets/images/orangeshpae1.png')}}" class="img-fluid w-100 oneShape" alt="img">
         <img src="{{asset('assets/images/orangeshpae2.png')}}" class="img-fluid w-100 twoShape" alt="img">
         <div class="container">
@@ -302,25 +312,27 @@
                     @endif
                 @endforeach
 
-                {{--                @foreach($products as $key => $item)--}}
-                {{--                    --}}{{--                    {{ dd($category->photo) }}--}}
-                {{--                    @if($key > 2)--}}
-                {{--                        @break--}}
-                {{--                    @endif--}}
-                {{--                    <div class="col-lg-4 col-sm-6">--}}
-                {{--                        <div class="product-box" data-aos="fade-right">--}}
-                {{--                            <div class="pro-img">--}}
-                {{--                                <a href="#">--}}
-                {{--                                    <img src="{{asset('assets/images/products/'.$item->photo) ?? 'Shop'}}" alt="img">--}}
-                {{--                                </a>--}}
-                {{--                            </div>--}}
-                {{--                            <h4>{{$item->category->name ?? 'Shop'}}</h4>--}}
-                {{--                            <p>{{$item->category->count()}} Products</p>--}}
-                {{--                        </div>--}}
-                {{--                    </div>--}}
-                {{--                @endforeach--}}
+                --}}{{--SHIRT SECTION 2 START--}}{{--
 
-                {{--<div class="col-md-6" data-aos="fade-right">
+                --}}{{--                @foreach($products as $key => $item)--}}{{--
+                --}}{{--                    --}}{{----}}{{--                    {{ dd($category->photo) }}--}}{{--
+                --}}{{--                    @if($key > 2)--}}{{--
+                --}}{{--                        @break--}}{{--
+                --}}{{--                    @endif--}}{{--
+                --}}{{--                    <div class="col-lg-4 col-sm-6">--}}{{--
+                --}}{{--                        <div class="product-box" data-aos="fade-right">--}}{{--
+                --}}{{--                            <div class="pro-img">--}}{{--
+                --}}{{--                                <a href="#">--}}{{--
+                --}}{{--                                    <img src="{{asset('assets/images/products/'.$item->photo) ?? 'Shop'}}" alt="img">--}}{{--
+                --}}{{--                                </a>--}}{{--
+                --}}{{--                            </div>--}}{{--
+                --}}{{--                            <h4>{{$item->category->name ?? 'Shop'}}</h4>--}}{{--
+                --}}{{--                            <p>{{$item->category->count()}} Products</p>--}}{{--
+                --}}{{--                        </div>--}}{{--
+                --}}{{--                    </div>--}}{{--
+                --}}{{--                @endforeach--}}{{--
+
+                --}}{{--<div class="col-md-6" data-aos="fade-right">
                     <div class="shirtBox">
                         <figure>
                             <img src="{{asset('assets/images/tshrt-dres.jpg')}}" class="img-fluid" alt="img">
@@ -355,10 +367,13 @@
                             <a href="{{route('front.category')}}">Sport<br>Wear</a>
                         </figure>
                     </div>
-                </div>--}}
+                </div>--}}{{--
+
+                    --}}{{--SHIRT SECTION 2 END--}}{{--
+
             </div>
         </div>
-    </section>
+    </section>--}}
 
 
     {{--    <section class="proSec">--}}
@@ -412,11 +427,15 @@
     {{--        </div>--}}
     {{--    </section>--}}
 
-    <section class="categorySec">
+    {{--SHIRT SECTION START--}}
+
+    {{--CATEGORY SECTION START--}}
+
+    {{--<section class="categorySec">
         <div class="container">
             <div class="row">
                 @foreach($categories as $category)
-                    {{--                                                        {{ dd($category->products) }}--}}
+                    --}}{{--                                                        {{ dd($category->products) }}--}}{{--
                     @if(count($category->subs) > 0)
                         <div class="col-md-4">
                             <div class="mainCatBox">
@@ -460,7 +479,10 @@
                 @endforeach
             </div>
         </div>
-    </section>
+    </section>--}}
+
+    {{--CATEGORY SECTION END--}}
+
     <section class="signupSec">
         <div class="container">
             <div class="row justify-content-center">
