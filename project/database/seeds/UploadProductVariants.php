e69dbd531cd795d50360752523ef1c04e6831efe
Shayan <shayan.khan@olivedigital.com>
2023-11-01 04:54:58 +0500

    save variations image

--- project/database/seeds/UploadProductVariants.php
+++ project/database/seeds/UploadProductVariants.php
@@ -7,7 +7,9 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\LazyCollection;
+use Illuminate\Support\Str;
 use League\Csv\Reader;
+use Image;
 
 class UploadProductVariants extends Seeder
 {
@@ -35,18 +37,26 @@ class UploadProductVariants extends Seeder
                 }
             });
 
-            $rows->each(function($row) {
+            $rows->each(function ($row) {
                 dump($row['product_id']);
                 $prd = Product::query()->where('sku', 'ae_' . $row['product_id'])->first();
                 if ($prd) {
+                    $fphoto = "";
+                    if ($row['option_image']) {
+                        $fimg = Image::make($row['option_image'])->resize(800, 800);
+                        $fphoto = time() . Str::random(8) . '.jpg';
+                        $fimg->save(public_path() . '/assets/images/variation/' . $fphoto);
+                    }
+
                     $prd->productVariants()->updateOrCreate([
                         "option_type" => $row['option_type'],
                         "option_id" => $row['option_id'],
                     ], [
                         "option_name" => $row['option_name'],
                         "option_display_name" => $row['option_display_name'],
-                        "option_image" => $row['option_image']
+                        "option_image" => $fphoto
                     ]);
+
                 } else {
                     Log::error("UploadProductVariants not found: " . $row['product_id']);
                 }
