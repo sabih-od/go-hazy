82d5c98b58244704d6810ccf36fdd46510439719
sheikh Sabih <sheikh.muhammad@olivedigital.com>
2024-04-26 03:07:01 +0500

    content changes

--- project/resources/views/alerts/admin/form-both.blade.php
+++ project/resources/views/alerts/admin/form-both.blade.php
@@ -1,6 +1,6 @@
       <div class="alert alert-success alert-dismissible validation" style="display: none;">
       <button type="button" class="close alert-close">&times;</button>
-            <p class="text-left"></p> 
+            <p class="text-left"></p>
       </div>
       <div class="alert alert-danger alert-dismissible validation" style="display: none;">
       <button type="button" class="close alert-close">&times;</button>
